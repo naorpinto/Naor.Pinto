2e8a4c4b53354f43ae00bacf2e2c9111b8dbd8d1
naorpinto <naorpinto6@gmail.com>
2021-11-19 21:13:51 +0200

    exercise5

---
+++ CVgrid.js
@@ -0,0 +1,6 @@
+var myName="Naor Pinto";
+console.log(myName);
+
+function toSaveCV(){
+    document.getElementById("Button").innerHTML="Saved"
+}
---
+++ animation1.js
@@ -0,0 +1,11 @@
+function thankyou(){
+    document.getElementById("Button").innerHTML="Thank You"
+    console.log("thank you")
+}
+
+var x=5,y=7,T=(x==y);
+console.log(T);
+
+function changePic(){
+    document.getElementById("idImage").src=""
+}
